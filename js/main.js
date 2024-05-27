64029dddad385d73645678e0308974e2fb3204b3
dilson.manjenje <dilson.manjenje@outlook.com>
2024-05-27 15:32:35 +0100

    #10 Collapsibles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((item) =>
+  item.addEventListener('click', function () {
+    this.classList.toggle('collapsible--expanded');
+  })
+);
+
